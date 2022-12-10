976170015dcb2a86cb17a090d63fe191ba818334
Nour-eddine <92918272+F-nour@users.noreply.github.com>
2022-12-10 16:56:19 +0100

    implement method on service

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap',
@@ -11,7 +12,7 @@ export class FaceSnapComponent implements OnInit {
   @Input() faceSnap!: FaceSnap;
   buttonText!: string;
 
-
+  constructor(private faceSnapsService: FaceSnapsService) { }
 
   ngOnInit(): void {
     this.buttonText = 'Oh Snap!';
@@ -19,10 +20,10 @@ export class FaceSnapComponent implements OnInit {
 
   onSnap() {
     if (this.buttonText === 'Oh Snap!') {
-      this.faceSnap.snaps++;
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap')
       this.buttonText = 'Oops, unSnap!';
     } else {
-      this.faceSnap.snaps--;
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unSnap')
       this.buttonText = 'Oh Snap!';
     }
   }
--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -8,6 +8,7 @@ export class FaceSnapsService {
 
   faceSnaps: FaceSnap[] = [
     {
+      id: 1,
       title: 'Kamel',
       description: 'Mon meilleur ami depuis tout petit !',
       imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
@@ -15,6 +16,7 @@ export class FaceSnapsService {
       snaps: 47
     },
     {
+      id: 2,
       title: 'Three Rock Mountain',
       description: 'Un endroit magnifique pour les randonnées.',
       imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
@@ -22,6 +24,7 @@ export class FaceSnapsService {
       snaps: 138
     },
     {
+      id: 3,
       title: 'Un bon repas',
       description: 'Mmmh que c\'est bon !',
       imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
@@ -29,4 +32,20 @@ export class FaceSnapsService {
       snaps: 1093
     }
   ]
+
+  getAllFaceSnaps(): FaceSnap[] { return this.faceSnaps }
+
+  getFaceSnapById(faceSnapId: number): FaceSnap {
+    const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId)
+    if (!faceSnap) {
+      throw new Error("Le FaceSnap n'existe pas")
+    } else {
+      return faceSnap;
+    }
+  }
+
+  snapFaceSnapById(faceSnapId: number, snapType: string): void {
+    const faceSnap = this.getFaceSnapById(faceSnapId)
+    snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--
+  }
 }
