297a1944f7e581f8654e5c7f3e77ec38615b4ddc
Nour-eddine <92918272+F-nour@users.noreply.github.com>
2022-12-10 15:43:22 +0100

    add conditions to display face-snaps

--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,10 +1,9 @@
 export class FaceSnap {
+    title!: string;
+    description!: string;
+    createdDate!: Date;
+    snaps!: number;
+    imageUrl!: string;
+    location?: string
 
-    constructor(public title: string,
-        public description: string,
-        public creationDate: Date,
-        public snaps: number,
-        public imageUrl: string
-    ) {
-    }
 }
