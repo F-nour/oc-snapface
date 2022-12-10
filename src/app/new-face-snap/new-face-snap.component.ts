b96f66b9c52734b24a33de1f4f8d66df9f6049b5
Nour-eddine <92918272+F-nour@users.noreply.github.com>
2022-12-10 19:44:15 +0100

    update reactiveForm

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-new-face-snap',
@@ -9,6 +11,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class NewFaceSnapComponent implements OnInit {
 
   snapForm!: FormGroup;
+  faceSnapPreview$!: Observable<FaceSnap>;
+
 
 
   constructor(private formBuilder: FormBuilder) { }
@@ -20,6 +24,15 @@ export class NewFaceSnapComponent implements OnInit {
       imageUrl: [null],
       location: [null]
     })
+
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map((formValue) => ({
+          ...formValue,
+          createdDate: new Date(),
+          snaps: 0,
+          id: 0
+      }))
+  );
   }
 
   onSubmitForm() {
