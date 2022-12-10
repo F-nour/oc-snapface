f7193a4e3ee829c3535a57ecc1841583ddaaab90
Nour-eddine <92918272+F-nour@users.noreply.github.com>
2022-12-10 15:15:26 +0100

    add event for snap button

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -12,6 +12,8 @@ export class FaceSnapComponent implements OnInit {
   creationDate!: Date;
   snaps!: number;
   imageUrl!: string;
+  buttonText!: string;
+
 
 
   ngOnInit(): void {
@@ -20,6 +22,16 @@ export class FaceSnapComponent implements OnInit {
     this.creationDate = new Date();
     this.snaps = 6;
     this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.buttonText = 'Oh Snap!';
   }
 
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.snaps++;
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.snaps--;
+      this.buttonText = 'Oh Snap!';
+    }
+  }
 }
